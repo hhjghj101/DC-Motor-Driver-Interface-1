faaa6536744e950253d3f26958610a53e5147b50
Keewitz <97384711+Keewitz@users.noreply.github.com>
2023-01-28 21:51:56 +0700

    Update TopModule.vhd

--- TopModule.vhd
+++ TopModule.vhd
@@ -10,45 +10,51 @@ entity TopModule is
 		Reset     : in STD_LOGIC;
 		Clock     : in STD_LOGIC;
 		CompOut   : out STD_LOGIC;
-		Direction : out STD_LOGIC);
+		Direction : out STD_LOGIC
+	     );
 end TopModule;
 
 architecture Behavioral of TopModule is
 	
 	component ClockDivider
-		port ( Clock 	  : in STD_LOGIC;
-		      Reset 	  : in STD_LOGIC;
-		      ClockDivOut : out STD_LOGIC
+		port ( 
+			Clock 	    : in STD_LOGIC;
+			Reset 	    : in STD_LOGIC;
+			ClockDivOut : out STD_LOGIC
 		     );
 	end component;
 
 	component BistableButton
-		port ( ButtonIn : in STD_LOGIC;
-		      Clock 	: in STD_LOGIC;
-		      Reset 	: in STD_LOGIC;
-		      ButtonOut : out STD_LOGIC
+		port (
+			ButtonIn  : in STD_LOGIC;
+			Clock 	  : in STD_LOGIC;
+			Reset 	  : in STD_LOGIC;
+			ButtonOut : out STD_LOGIC
 		     );
 	end component;
 
 	component CounterA
-		port ( Clock : in STD_LOGIC;
-		      Reset  : in STD_LOGIC;
-		      CE     : in  STD_LOGIC;
-		      CoutA  : out STD_LOGIC_VECTOR(3 downto 0)
+		port (
+			Clock : in STD_LOGIC;
+			Reset  : in STD_LOGIC;
+			CE     : in  STD_LOGIC;
+			CoutA  : out STD_LOGIC_VECTOR(3 downto 0)
 		     );
 	end component;
 
 	component CounterB
-		port ( Clock : in STD_LOGIC;
-		      Reset : in STD_LOGIC;
-		      CoutB : out STD_LOGIC_VECTOR(3 downto 0)
+		port (
+			Clock : in STD_LOGIC;
+			Reset : in STD_LOGIC;
+			CoutB : out STD_LOGIC_VECTOR(3 downto 0)
 		     );
 	end component;
 
 	component Comparator
-		port ( CinA 	: in STD_LOGIC_VECTOR(3 downto 0);
-		      CinB 	: in STD_LOGIC_VECTOR(3 downto 0);
-		      CompOut : out STD_LOGIC
+		port ( 
+			CinA 	: in STD_LOGIC_VECTOR(3 downto 0);
+			CinB 	: in STD_LOGIC_VECTOR(3 downto 0);
+			CompOut : out STD_LOGIC
 		     );
 	end component;
 
@@ -62,10 +68,11 @@ begin
 	
 	ClockDiv : ClockDivider 
 		port map
-			( Reset	     => Reset,
-			 Clock       => Clock,
-			 ClockDivOut => s_Clock
-			);
+			( 
+				Reset	     => Reset,
+				Clock       => Clock,
+			 ClodkDivOut => s_Clock
+			);  
 
 	BButton : BistableButton
 		port map
