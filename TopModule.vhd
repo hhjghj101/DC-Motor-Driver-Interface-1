086333af344c2c0b30f113573a6c7837f3b8046e
Keewitz <97384711+Keewitz@users.noreply.github.com>
2023-01-28 21:47:37 +0700

    Update TopModule.vhd

--- TopModule.vhd
+++ TopModule.vhd
@@ -16,49 +16,40 @@ end TopModule;
 architecture Behavioral of TopModule is
 	
 	component ClockDivider
-		port (
-		Clock 		: in STD_LOGIC;
-		Reset 		: in STD_LOGIC;
-		ClockDivOut : out STD_LOGIC
-		);
+		port ( Clock 	  : in STD_LOGIC;
+		      Reset 	  : in STD_LOGIC;
+		      ClockDivOut : out STD_LOGIC
+		     );
 	end component;
 
 	component BistableButton
-		port
-			( 
-				ButtonIn  : in STD_LOGIC;
-				Clock 	 : in STD_LOGIC;
-				Reset 	 : in STD_LOGIC;
-				ButtonOut : out STD_LOGIC
-			);
+		port ( ButtonIn : in STD_LOGIC;
+		      Clock 	: in STD_LOGIC;
+		      Reset 	: in STD_LOGIC;
+		      ButtonOut : out STD_LOGIC
+		     );
 	end component;
 
 	component CounterA
-		port
-			(
-				Clock  : in STD_LOGIC;
-				Reset  : in STD_LOGIC;
-				CE 	 : in  STD_LOGIC;
-				CoutA  : out STD_LOGIC_VECTOR(3 downto 0)
-			);
+		port ( Clock : in STD_LOGIC;
+		      Reset  : in STD_LOGIC;
+		      CE     : in  STD_LOGIC;
+		      CoutA  : out STD_LOGIC_VECTOR(3 downto 0)
+		     );
 	end component;
 
 	component CounterB
-		port
-			( 
-				Clock : in STD_LOGIC;
-				Reset : in STD_LOGIC;
-				CoutB : out STD_LOGIC_VECTOR(3 downto 0)
-			);
+		port ( Clock : in STD_LOGIC;
+		      Reset : in STD_LOGIC;
+		      CoutB : out STD_LOGIC_VECTOR(3 downto 0)
+		     );
 	end component;
 
 	component Comparator
-		port
-			( 
-				CinA 	: in STD_LOGIC_VECTOR(3 downto 0);
-				CinB 	: in STD_LOGIC_VECTOR(3 downto 0);
-				CompOut : out STD_LOGIC
-			);
+		port ( CinA 	: in STD_LOGIC_VECTOR(3 downto 0);
+		      CinB 	: in STD_LOGIC_VECTOR(3 downto 0);
+		      CompOut : out STD_LOGIC
+		     );
 	end component;
 
 	signal s_Clock  : STD_LOGIC; 
@@ -71,44 +62,39 @@ begin
 	
 	ClockDiv : ClockDivider 
 		port map
-			(
-				Reset 		=> Reset,
-				Clock       => Clock,
-				ClockDivOut => s_Clock
+			( Reset	     => Reset,
+			 Clock       => Clock,
+			 ClockDivOut => s_Clock
 			);
 
 	BButton : BistableButton
 		port map
-			( 
-				ButtonIn  => Button,
-				Clock 	 => s_Clock,
-				Reset 	 => Reset,
-				ButtonOut => s_Button
+			( ButtonIn  => Button,
+			 Clock 	 => s_Clock,
+			 Reset 	 => Reset,
+			 ButtonOut => s_Button
 			);
 
 	Counter1 : CounterA
 		port map
-			(
-				Clock => s_Clock,
-				Reset => Reset,
-				CE 	 => s_Button,
-				CoutA => s_CoutA
+			( Clock => s_Clock,
+			 Reset  => Reset,
+			 CE     => s_Button,
+			 CoutA  => s_CoutA
 			);
 
 	Counter2 : CounterB 
 		port map
-			( 
-				Clock => s_Clock,
-				Reset => Reset,
-				CoutB => s_CoutB
+			( Clock => s_Clock,
+			 Reset  => Reset,
+			 CoutB  => s_CoutB
 			);
 
 	Comparator1 : Comparator 
 		port map
-			(
-				CinA => s_CoutA,
-				CinB => s_CoutB,
-				CompOut => CompOut
+			( CinA => s_CoutA,
+			 CinB => s_CoutB,
+			 CompOut => CompOut
 			);
-
+		
 end Behavioral;
